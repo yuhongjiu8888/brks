2570f25d391ff018632a636b7ce718d4ffe6f739
yuwei <yuhongjiu@foxmail.com>
2018-11-17 11:40:35 +0800

    new push

---
+++ conf/config.lua
@@ -0,0 +1,6 @@
+mysqldb = {	host = "127.0.0.1",
+			port = 3306,
+			dbuser = "root",
+			dbpasswd = "Abc123456",
+			dbname = "dongnaobike" }
+serverPort = 9090
---
+++ src/service/CMakeFiles/service.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/yuwei/gitjob/brks/src/service/DispatchMsgService.cpp" "/home/yuwei/gitjob/brks/src/service/CMakeFiles/service.dir/DispatchMsgService.cpp.o"
+  "/home/yuwei/gitjob/brks/src/service/MsgQueue.cpp" "/home/yuwei/gitjob/brks/src/service/CMakeFiles/service.dir/MsgQueue.cpp.o"
+  "/home/yuwei/gitjob/brks/src/service/bike_service.cpp" "/home/yuwei/gitjob/brks/src/service/CMakeFiles/service.dir/bike_service.cpp.o"
+  "/home/yuwei/gitjob/brks/src/service/sqlconnection.cpp" "/home/yuwei/gitjob/brks/src/service/CMakeFiles/service.dir/sqlconnection.cpp.o"
+  "/home/yuwei/gitjob/brks/src/service/user_service.cpp" "/home/yuwei/gitjob/brks/src/service/CMakeFiles/service.dir/user_service.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../third/include"
+  "./service"
+  "./intf"
+  "./common"
+  "./bus"
+  "service/../../third/include"
+  "service/../common"
+  "service/../bus"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
